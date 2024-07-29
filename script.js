b142cc8461cd201f12b9c49579297c59c08700ad
Invincible-Atlas <148077776+Invincible-Atlas@users.noreply.github.com>
2024-07-29 15:21:21 +0000

    designed the basic filesystem framework

---
+++ script.js
@@ -0,0 +1,21 @@
+// the JSON representation of the file tree that will be used to generate the page
+const tree = [{"name":"file1.file"},{"name":"file2.file"},{"name":"folder1","contents":[{"name":"file3.file"}]}]
+function generate(tree){
+    let fsContainer = document.createElement("div");
+    fsContainer.classList = "fsContainer";
+    for(i in tree){
+        let 
+        if(i.contents){
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
